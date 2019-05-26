0abb1ef06d959047503e72e85c4e0ace412a5da9
Adam Häggström <adh01@hotmail.se>
2019-05-26 12:54:08 +0200

    .

--- SubGame/Elements/HelicopterElement.cs
+++ SubGame/Elements/HelicopterElement.cs
@@ -18,18 +18,24 @@ namespace SubGame.Elements
         }
 
         public override void LoadContent(ContentManager aContentManager, string anAsset)
-        {
-            base.LoadContent(aContentManager, anAsset);
-        }
+            => base.LoadContent(aContentManager, anAsset);
 
         public override void Update(GameTime aGameTime)
         {
-            base.Update(aGameTime);
+
         }
 
         public override void Draw(SpriteBatch aSpriteBatch)
         {
-            base.Draw(aSpriteBatch);
+            Rectangle mySourceRectangle = new Rectangle(0, 0, myTexture.Width, myTexture.Height);
+            Vector2 myOrigin = new Vector2(0, 0);
+            SpriteEffects myEffects = SpriteEffects.None;
+            if (AccessDirection < 0.0f)
+            {
+                myEffects = SpriteEffects.FlipHorizontally;
+            }
+
+            aSpriteBatch.Draw(myTexture, AccessPosition, mySourceRectangle, Color.White, AccessRotation, myOrigin, AccessScale, myEffects, 1);
         }
     }
 }
